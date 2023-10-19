f53cdd2206c46b538c5f60047dc0355d78a88556
Aidan Harvey <aidanharveydev@gmail.com>
2023-10-19 11:32:34 -0700

    Added in choosing draft functionality #5

--- server.js
+++ server.js
@@ -111,10 +111,73 @@ app.get('/draft', async (req, res) => {
         // Increment i
         i++
     }
-    if(type === 'monsters')
-        console.log(data)
     res.send(data)
 })
+
+// Routes for selecting draft picks
+app.post('/pick/adventurers', selectDraftPick('adventurers'))
+app.post('/pick/traps', selectDraftPick('traps'))
+app.post('/pick/monsters', selectDraftPick('monsters'))
+app.post('/pick/bosses', selectDraftPick('bosses'))
+
+// Generic function for selecting a pick
+function selectDraftPick(type) {
+    // Return the function that uses the name
+    return async function (req, res) {
+        // Authenticate with google sheets
+        const serviceAccountAuth = new JWT({
+            email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || config.google.email,
+            key: process.env.GOOGLE_PRIVATE_KEY || config.google.key,
+            scopes: [
+                'https://www.googleapis.com/auth/spreadsheets',
+            ],
+        })
+        // Get the google sheet
+        const doc = new GoogleSpreadsheet('1uQd7Xx5HGGmdinT9Rret-IS0bZHQeBEPePrGM5-Patg', serviceAccountAuth);
+        // Load the sheet data
+        await doc.loadInfo()
+        // Get the sheet for the correct draft
+        const draftSheet = await doc.sheetsByTitle[type.charAt(0).toUpperCase() + type.slice(1) + ' Draft']
+        // Load cells from the sheet
+        await draftSheet.loadCells('A1:H100')
+        // Start at the top of the sheet and process downward
+        let i = 0
+        while (i < 100) {
+            // Get the current cell
+            const playerCell = draftSheet.getCell(i, 0)
+            const nameCell = draftSheet.getCell(i, 1)
+            // Check for empty cells
+            if (!playerCell.formattedValue) {
+                // Exit at end of table
+                if (!nameCell.formattedValue) {
+                    break
+                }
+                // Check if the current cell is the requested pick
+                if (i - 1 == req.body.index) {
+                    // Check if the cell has already been picked
+                    if (!!playerCell.value) {
+                        res.sendStatus(409)
+                        return
+                    }
+                    // Update the pick
+                    playerCell.value = req.body.player
+                }
+            } else { // Handle non-empty cells
+                // Check if the current cell has been picked
+                if (playerCell.formattedValue === req.body.player) {
+                    // Remove the pick
+                    playerCell.value = ''
+                }
+            }
+            // Increment i
+            i++
+        }
+        // Save updates to the sheet
+        await draftSheet.saveUpdatedCells()
+        // Return success
+        res.sendStatus(200)
+    }
+}
 
 app.listen(process.env.PORT || 80, () => {
     console.log(`Listening on ${process.env.PORT || 80}`)
